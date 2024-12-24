7c566a83e014147fa0f77b7d753e9f37aaaa7248
Joshua Koshy <joshuavkoshy@gmail.com>
2024-12-23 20:53:28 -0600

    add port of entry

--- News_Aggregator.py
+++ News_Aggregator.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import requests
 import json
@@ -35,4 +36,5 @@ def home():
     return render_template("index.html", articles=articles, json=json)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
